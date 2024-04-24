eea361c19b1dbe4f8707390b96c0ff8861b181b4
Lucas Barros <lcsavb@gmail.com>
2024-04-24 15:56:36 +0200

    progressing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
 use std::process::Command;
+use std::collections::HashMap;
+
+let base_numbers: HashMap<u64, String> = HashMap::new();
+
+let base_numbers = [(0, "zero"), (1, "one", 1), (2, "two", 2), (3, "three", 3), (4, "four", 4), 
+                (5, "five", 5), (6, "six", 6), (7, "seven", 7), (8, "eight", 8), 
+                (9, "nine", 9), (10, "ten", 10), (11, "eleven", 11), (12, "twelve", 12), 
+                (13, "thirteen", 13), (14, "fourteen", 14), (15, "fifteen", 15), 
+                (16, "sixteen", 16), (17, "seventeen", 17), (18, "eighteen", 18), (19, "nineteen", 19)];
+
+let tens: HashMap<u64, String> = HashMap::new();
+
+let tens = [(20, "twenty", 20), (30, "thirty", 30), (40, "forty", 40), (50, "fifty", 50), 
+            (60, "sixty", 60), (70, "seventy", 70), (80, "eighty", 80), (90, "ninety", 90)];
+
+
 
 pub fn encode(n: u64) -> String {
     match n.len() {
-        2 => 
+        1 | 2 => {
+            return numbers.get(&n).unwrap().to_string()
+        
     }
 }
 
