41ee1e76c804be37d270956013732afd7c3d6a3a
Lucas Barros <lcsavb@gmail.com>
2024-04-24 15:51:15 +0200

    starting the work

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+use std::process::Command;
+
 pub fn encode(n: u64) -> String {
-    unimplemented!("Say {n} in English.");
+    match n.len() {
+        2 => 
+    }
+}
+
+
+pub fn speak(n: &str) -> String {
+    let output = Command::new("espeak")
+        .arg(n)
+        .output()
+        .expect("failed to execute process");
+
+    String::from_utf8(output.stdout).unwrap()
 }
